eb466ecb52bdeda58eeed99c9f5eda5846462c63
Namhun Kim <nhk9680@gmail.com>
2020-01-08 15:04:08 +0900

    automatize article index
    - 게시글 목차 위치 갱신 자동화
    - 기타 코드 명료화

--- joonggonara.py
+++ joonggonara.py
@@ -5,66 +5,21 @@ from winsound import Beep
 import telegram
 import os
 
+#telegram
 my_token = '837339362:AAESYsiM7S5qRu4SBGYK-OLhrEgRtPWgDcA'
-bot = telegram.Bot(my_token)
-chat_id = bot.getUpdates()[-1].message.chat.id
-text_tmp = bot.getUpdates()[-1].message.text
+bot         = telegram.Bot(my_token)
+chat_id     = bot.getUpdates()[-1].message.chat.id
+text_tmp    = bot.getUpdates()[-1].message.text
 
+#selenium.webdriver
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument('disable-gpu')
-
-'''
-log-level: 
-Sets the minimum log level.
-Valid values are from 0 to 3: 
-
-    INFO = 0, 
-    WARNING = 1, 
-    LOG_ERROR = 2, 
-    LOG_FATAL = 3.
-
-default is 0.
-'''
 options.add_argument('log-level=1')
-
 
 driver = webdriver.Chrome(executable_path='D:/chromedriver.exe', options=options)
 driver.implicitly_wait(1)
-
-title_tmp = ' '
-author_tmp = ' '
-#info = (title_tmp, author_tmp)
-'''
-blacklist = ['☆', '★', '◀', '▶', '■', '□', '●', '○', '◇', '◈', '♪', '♨', '♣', '♧', '⊙', '#', '@', '<', '>', \
-            '삽니다', '매입', '파손', '고장', '최고가', '최상', \
-            '최저가', '할인', '대박', '세일', '한정', '특', '특급', '특가', '최대한', '저렴하게', 'S급', 's급', '싸게', \
-            '전국', '택배', '가개통', '선택', '약정', '전문', '강변', '출장', '급처', '사은품', '최고회원', \
-            '부산', '수원', '대전', '대구', '전주', '청주']
-
-block_user = ['모바일', '폰', 'phone', 'Phone', '남포폰깨비', '햄툐리a', 'diddud1234', '후애마이', 'foreversoo1', '럭키쵸이', '안전거래직', '난야똑또기', '절대신용7', '원하다폰', 'kg5161', '카나리비오', '중고폰', 'baksaphone', 'chsekfr77', '깔끔하게쿨하게', '아나바다폰', 'rhden2626', '45351', 'rnjstnsgud1487', '몽몽이58', 'Hey', 'jhpa', '르샤트', '강아징아아', '꼬마', 'wjdnf33', 'hiplaza1989', '6106모바일2', 'rnjstnsgud1487', 'superlueh4', 'Smile Phone', '깜장발이', '금빛돼지']
-'''
-
-'''
-f=open("blacklist.txt", 'r')
-blacklist=[]
-while True:
-    line=f.readline()
-    if not line:
-        break
-    blacklist.append(line[:-1])
-f.close()
-
-f=open("block_user.txt", 'r')
-block_user=[]
-while True:
-    line=f.readline()
-    if not line:
-        break
-    block_user.append(line[:-1])
-f.close()
-'''
 
 def fopen_r(filename):
     f=open(filename, 'r')
@@ -77,44 +32,51 @@ def fopen_r(filename):
     f.close()
     return _list
 
-blacklist=fopen_r("blacklist.txt")
-block_user=fopen_r("block_user.txt")
+blacklist   = fopen_r("blacklist.txt")
+block_user  = fopen_r("block_user.txt")
+count       = fopen_r("count.txt")
 
-count=fopen_r("count.txt")
+individual  = int(count[0])
+vender      = int(count[1])
 
-individual = int(count[0])
-vender = int(count[1])
+keywords    = ['V30', 'v30', 'G7', 'g7']
+sign        = ['↑', '↗', '→', '↘', '↓', '↙', '←', '↖']
 
+title_tmp   = ' '
+author_tmp  = ' '
 
+#get article index
+driver.get('https://cafe.naver.com/ArticleList.nhn?search.clubid=10050146&search.menuid=424&search.boardtype=L')
+driver.switch_to.frame('cafe_main')
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
 
-keywords = ['V30', 'v30', 'G7', 'g7']
-
-sign=['↑', '↗', '→', '↘', '↓', '↙', '←', '↖']
+articleIndex    = 0
+while True:
+    try:
+        postnum=soup.select(
+            '#main-area > div:nth-child('+str(articleIndex)+') > table > tbody > tr:nth-child(1) > td.td_article > div.board-number > div').pop().text.strip()
+        print("Got it!")
+        break
+    except:
+        print(str(articleIndex), end='\r')
+        articleIndex+=1
+        continue
 
 t=0
 while True:
     driver.get('https://cafe.naver.com/ArticleList.nhn?search.clubid=10050146&search.menuid=424&search.boardtype=L')
     driver.switch_to.frame('cafe_main')
-
-    # print(driver.page_source)
-
-    # inner_elem=driver.find_elements_by_class_name('m-tcol-c')
-
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    # pop from empty list 오류 발생 시
-
-    # 공지사항 리스트 수 변경으로 div:nth-child() 안의 숫자가 바뀔 경우 
-    # 게시글 selector 다시 추출해서 재설정하는게 빠르고 편함
-
     try:
         postnum=soup.select(
-            '#main-area > div:nth-child(7) > table > tbody > tr:nth-child(1) > td.td_article > div.board-number > div').pop().text.strip()
+            '#main-area > div:nth-child('+str(articleIndex)+') > table > tbody > tr:nth-child(1) > td.td_article > div.board-number > div').pop().text.strip()
         title = soup.select(
-            '#main-area > div:nth-child(7) > table > tbody > tr:nth-child(1) > td.td_article > div.board-list > div > a').pop().text.strip()
+            '#main-area > div:nth-child('+str(articleIndex)+') > table > tbody > tr:nth-child(1) > td.td_article > div.board-list > div > a').pop().text.strip()
         author = soup.select(
-            '#main-area > div:nth-child(7) > table > tbody > tr:nth-child(1) > td.td_name > div > table > tbody > tr > td > a').pop().text.strip()
+            '#main-area > div:nth-child('+str(articleIndex)+') > table > tbody > tr:nth-child(1) > td.td_name > div > table > tbody > tr > td > a').pop().text.strip()
     except Exception as e:
         print(e)
         bot.sendMessage(chat_id=chat_id, text=str(e))
